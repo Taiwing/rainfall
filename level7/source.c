d78100575046ce6e47fc620154874c7163d0e394
yoann foreau <yoann.foreau@ito.am>
2022-07-06 04:34:57 +0200

    docs: remove useless comments in source

--- level7/source.c
+++ level7/source.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Global variable (not sure about that) set to NULL at first
 char	*c;
 
 void	m(int param_1, int param_2, int param_3, int param_4, int param_5)
@@ -25,9 +24,7 @@ int		main(int argc, char **argv)
 	alloc2 = (int *)malloc(8);
 	alloc2[0] = 2;
 	alloc2[1] = malloc(8);
-	// First possible overflow, try to overwrite c with a valid value
 	strcpy((char *)alloc1[1], argv[1]);
-	// Second possible overflow, try to overwrite puts GOT address to m
 	strcpy((char *)alloc2[1], argv[2]);
 	stream = fopen("/home/user/level8/.pass", "r");
 	fgets(c, 0x44, stream);
