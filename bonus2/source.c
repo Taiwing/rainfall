99e0ef2070e62dc741bef47fa92e8b96f5128793
yoann foreau <yoann.foreau@ito.am>
2022-07-10 11:08:40 +0200

    feat: start decompiling

---
+++ bonus2/source.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+
+int		language;
+
+void	greetuser(char *user_input)
+{
+	char	greeting[80];
+
+	if (language == 1)
+		strcpy(greeting, "Hyvää päivää ");
+	else if (language == 2)
+		strcpy(greeting, "Goedemiddag! ");
+	else if (language == 0)
+		strcpy(greeting, "Hello ");
+	strcat(greeting, user_input);
+	puts(greeting);
+}
+
+int		main(int argc, char **argv)
+{
+	char	*locale;
+	char	buffer[144];
+
+	if (argc == 3) {
+		bzero(buffer + 80, 19);
+		strncpy(buffer + 80, argv[1], 40);
+		strncpy(buffer + 120, argv[2], 32);
+		if ((locale = getenv("LANG")) != NULL) {
+			if (!memcmp(locale, "fi", 2))
+				language = 1;
+			else if (!memcmp(locale, "nl", 2))
+				language = 2;
+		}
+		// THIS THE THING: the memcpy is not on 19 bytes but 19 words!
+		// That explains the overflow in the greet_user function.
+		memcpy(buffer, buffer + 80, 19 * 4);	// 19 * 4 = 76 bytes
+		greetuser(buffer);
+		return (0);
+	}
+	return (1);
+}
